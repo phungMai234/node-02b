2a5642a8f1ae44612a233aa6b5efda2252cefe31
phungMai234 <phungtuyet234@gmail.com>
2019-04-14 19:57:30 +0700

    5th

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-let http = require('http');
+const http = require('http');
 
 const port = process.env.PORT;
 const host = process.env.HOST;
 
-function methodGet() {
-    return 'world';
-
-}
-function methodPut() {
-    return 'world updated';
-
-}function methodPost() {
-    return 'world created';
-
-}function methodDelete() {
-    return 'world deleted';
-
-}
 var server = http.createServer(function(req, res)
     {
         res.writeHead(200, {"Content-type" : "text/html"});
+
         if(req.method == 'GET' && req.url == '/hello')
         {
             res.write('world');
